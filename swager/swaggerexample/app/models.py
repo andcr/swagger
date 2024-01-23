41a321024dd5b0eaad266372138dac1765b41399
andcr <fatxullohibrohim@gmail.com>
2024-01-23 21:37:57 +0500

    first commi

---
+++ swager/swaggerexample/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Category(models.Model):
+    categoryname = models.CharField(max_length=40, null=False, blank=False)
+
+    def __str__(self) -> str:
+        return self.categoryname
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30, null=True,blank=True)
+    price = models.CharField(max_length=40,null=True, blank=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
