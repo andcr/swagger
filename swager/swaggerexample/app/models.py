5e28abb881bb2f197e8585dd704c690d2be94028
andcr <fatxullohibrohim@gmail.com>
2024-01-25 23:23:14 +0500

    queue1

--- swager/swaggerexample/app/models.py
+++ swager/swaggerexample/app/models.py
@@ -16,3 +16,23 @@ class Product(models.Model):
 
     def __str__(self) -> str:
         return self.name
+class MyQueue:
+  def __init__(self):
+    self.input = []
+    self.output = []
+
+  def push(self, x: int) -> None:
+    self.input.append(x)
+
+  def pop(self) -> int:
+    self.peek()
+    return self.output.pop()
+
+  def peek(self) -> int:
+    if not self.output:
+      while self.input:
+        self.output.append(self.input.pop())
+    return self.output[-1]
+
+  def empty(self) -> bool:
+    return not self.input and not self.output
